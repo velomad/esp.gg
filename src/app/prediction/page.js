4b06fcdaf62b7e4dbb1536e6375991cc0eaeb350
Sagar Chavan <sagar.chavan@sportzinteractive.net>
2024-10-26 17:20:57 +0530

    feat:added drag elements

---
+++ src/app/prediction/page.js
@@ -0,0 +1,12 @@
+import { SortableList } from "@/components/ui/sortable-list";
+import React from "react";
+
+const Prediction = () => {
+  return (
+    <div>
+      <SortableList />
+    </div>
+  );
+};
+
+export default Prediction;
